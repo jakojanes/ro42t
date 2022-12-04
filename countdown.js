ae1c3072785cf73bc83cf85d7da132a0395438be
jakojanes <jakojanes2@gmail.com>
2022-12-04 16:20:16 +0200

    Lisasin javascriptile commentid

--- countdown.js
+++ countdown.js
@@ -1,5 +1,9 @@
-var end = new Date('02/19/2032 10:1 AM');
 
+/*Kood saadud: https://www.w3schools.com/howto/howto_js_countdown.asp, koodi meie poolt kohandatud*/
+
+
+var end = new Date('05/03/2023 12:00 AM'); /*Mis kuupäevani countdown on*/
+    /*Teen muutujad, et hiljem arvutada sekundeid jne...*/
     var _second = 1000;
     var _minute = _second * 60;
     var _hour = _minute * 60;
@@ -7,20 +11,21 @@ var end = new Date('02/19/2032 10:1 AM');
     var timer;
 
     function showRemaining() {
-        var now = new Date();
-        var distance = end - now;
-        if (distance < 0) {
+        var now = new Date(); /*Praegune kuupäev */
+        var distance = end - now; /*Kalkuleerin praeguse ja otsitava kuu vahe*/
+        if (distance < 0) { /*Kui kuupäev on möödas, siis kuvab Möödas*/
 
             clearInterval(timer);
-            document.getElementById('countdown').innerHTML = 'EXPIRED!';
+            document.getElementById('countdown').innerHTML = 'Möödas!';
 
             return;
         }
+        /*Kalkuleerin mitu sekundit, minutit, tundi ja päeva on valimisteni*/
         var days = Math.floor(distance / _day);
         var hours = Math.floor((distance % _day) / _hour);
         var minutes = Math.floor((distance % _hour) / _minute);
         var seconds = Math.floor((distance % _minute) / _second);
-
+        /*Kuvab arvutatud väärtused HTMLi failis (veebilehel)*/
         document.getElementById('countdown').innerHTML = days + ' päeva ';
         document.getElementById('countdown').innerHTML += hours + ' tundi ';
         document.getElementById('countdown').innerHTML += minutes + ' minutit ';
